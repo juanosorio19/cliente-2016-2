60052cd250c11cc02acbc5096f823e770b802c85
juanosorio19 <josorio19@utp.edu.co>
2016-08-23 00:10:08 -0500

    añadido determinante

--- client.cpp
+++ client.cpp
@@ -1,15 +1,98 @@
 #include <iostream>
 #include <zmqpp/zmqpp.hpp>
+#include <math.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 using namespace zmqpp;
 
+
+
+
 int main(int argc, char const *argv[]) {
-  if (argc != 4) {
+  string op=argv[1];
+  if (argc != 2) {
     cerr << "Error calling the program" << endl;
     return 1;
   }
 
+
+  if (argv[1]="producto")
+  {
+
+   int data;           // file contains an undermined number of integer values
+   ifstream fin;     // declare stream variable name
+
+   int rows1,cols1,rows2,cols2;
+
+   fin.open("myfile.txt",ios::in);    // open file
+       
+   fin >> data;  
+   rows1=data;      
+   fin >> data; 
+   cols1=data; 
+   string  matrix1,matrix2;
+
+   int size=cols1*rows1;
+   
+   int i=0;
+                            
+   while ( i<size )      //if not at end of file, continue reading numbers
+   {
+        //print numbers to screen
+    int tem=10;
+
+     fin >> data;
+      
+      //cout<<data;
+      string temp=to_string(data);
+
+      
+
+     
+     
+
+     matrix1+=temp; //get next number from file
+     matrix1+=',';//tokenize numbers
+     i+=1;
+
+
+
+   }
+     i=0;
+     fin >> data;
+     rows2=data;
+     fin >> data;
+     cols2 = data;
+     int size2=cols2*rows2;
+     
+     while (!fin.eof( ) && i<size2  )      //if not at end of file, continue reading numbers
+   {
+        //print numbers to screen
+     fin >> data;
+     string temp=to_string(data);
+
+     
+     matrix2+=temp; //get next number from file
+     matrix2+=',';//tokenize numbers
+     
+     
+     i+=1;
+
+   }
+
+
+     fin.close( );       //close file
+
+  
+  
+    cout<<matrix1<<endl;
+    cout<<matrix2<<endl;
+
+ 
+  
+   
+   
   context ctx;
   socket s(ctx, socket_type::request);
 
@@ -17,21 +100,30 @@ int main(int argc, char const *argv[]) {
   s.connect("tcp://localhost:4242");
 
   string op(argv[1]);
-  int op1 = atoi(argv[2]);
-  int op2 = atoi(argv[3]);
-
+ // int op1 = atoi(argv[2]);
+ // int op2 = atoi(argv[3]);
+   
   message req;
-  req << op << op1 << op2;
+  req << op << rows1 << cols1 << rows2 << cols2 << matrix1 << matrix2;
   s.send(req);
   cout << "Request sent." << endl;
 
   message rep;
   s.receive(rep);
-  int result = 0;
+  string result;
   rep >> result;
   cout << "Response " << result << endl;
 
   cout << "Finished." << endl;
+
+
+  
+
+   
+  }
+
+
+
   return 0;
 }
 
--- server.cpp
+++ server.cpp
@@ -1,9 +1,82 @@
 #include <iostream>
 #include <string>
+#include <math.h>
 #include <zmqpp/zmqpp.hpp>
-
+#include <sstream>
 using namespace std;
 using namespace zmqpp;
+
+
+//creates matrix m[cols][rows]
+ vector< vector<int> > createMatrix(vector< vector<int> > matrix,int rows, int cols){
+
+  
+  matrix.resize(rows+1);
+
+  for (int i = 1; i <= rows; i++) {
+    matrix[i].resize(cols+1);
+  }
+return matrix;
+}
+
+/*
+   int aux = 0;
+  int count = 0;
+  for(int i = 0; i < op1; i++){
+    vector<int> x;
+      for(int j = 0; j < op2; j++){
+          stringstream ss;
+          ss << m[count];
+          ss >> aux;
+          x.push_back(aux);
+          count ++;
+    }
+    matrix1.push_back(x);x*/
+
+ vector< vector<int> > fillMatrix(string matrix,int rows,int cols){
+  vector< vector<int> > m;
+  vector< vector<int> > new_matrix=createMatrix(m,rows,cols);
+  int aux=0;
+  int count=0;
+for (int k = 0; k < matrix.size(); ++k)
+{
+  
+
+  for (int i = 1; i <= rows; i++)
+  {
+    for (int j = 1; j <= cols; j++)
+    {
+      if (matrix[k]!=',')
+      {
+        //aqui falta algo para hacer que llene el vector de vectores correctamente
+      
+      cout << "Ingrese el numero de la posicion " << i << " " << j <<" "<<matrix[k]<< endl;
+      //new_matrix[i][j]=stoi(matrix[k]);
+      }
+    }
+  }
+}
+return new_matrix;
+
+
+}
+
+ void printMatrix(vector< vector<int> > matrix,int rows,int cols){
+
+  for (int i = 1; i <= rows; i++)
+  {
+    for (int j = 1; j <= cols; j++)
+    {
+      
+      cout<< matrix[i][j]<<" ";
+    }
+    cout<<endl;
+  }
+
+
+
+
+}
 
 int main(int argc, char *argv[]) {
   // initialize the 0MQ context
@@ -22,16 +95,28 @@ int main(int argc, char *argv[]) {
     message req;
     s.receive(req);
 
-    string op;
-    int op1 = 0;
-    int op2 = 0;
-    req >> op >> op1 >> op2;
-    int result = op1 + op2;
+    string op,matrix1,matrix2;
+    int rows1,cols1,rows2,cols2;
+    rows1=cols1=rows2=cols2=0;
+
+    req >> op >> rows1 >> cols1 >> rows2 >> cols2 >> matrix1 >> matrix2;
+
+    vector< vector<int> > matriz=fillMatrix(matrix1,rows1,cols1);
+
+    printMatrix(matriz,rows1,cols1);
+
+
+
+
+
+    cout<<rows1<<endl;
+    
 
     message rep;
-    rep << result;
+    rep << rows1;
     s.send(rep);
-    cout << "Sent: " << result << endl;
+    cout << "Sent: " << rows1+5 << endl;
   }
   cout << "Finished." << endl;
 }
+
