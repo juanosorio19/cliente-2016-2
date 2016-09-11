128e980cb7d8018f3f04355ce326939c3a9a4fc4
juanosorio19 <josorio19@utp.edu.co>
2016-09-11 12:58:10 -0500

    Added support for secuential multMatrix

--- matrix/matrix.cc
+++ matrix/matrix.cc
@@ -13,7 +13,7 @@ class Matrix
 		 int cols;
 	public:
 		Matrix(){};
-		//Creates a empty matrix given from a .txt file
+		//Creates a matrix object given from a .txt file
 		Matrix(const string & file){
 			double iterator;           // file contains an undermined number of integer values
 		    ifstream fin;     // declare stream variable name
@@ -39,6 +39,18 @@ class Matrix
   				}
 
 		};
+		//constructor that creates an empty matrix of m*n 
+		Matrix(const int &m,const int &n){
+			rows=m;
+			cols=n;
+			data.resize(rows+1);
+			for (int i = 1; i <= rows; i++){
+				data[i].resize(cols+1);
+			}
+
+		};
+		int getCols(){return cols;}
+		int getRows(){return rows;}
 		void printMatrix(){
 			for (int i = 1; i <= rows; i++){
   				for (int j = 1; j <= cols; j++){
@@ -49,30 +61,26 @@ class Matrix
   			cout<<endl;
 
 		}
+		Matrix matrixMult(const Matrix & b){
+			//Assertion missed, a.cols and b.rows have to be equals 
+			Matrix result(this->rows,b.cols);
+
+			for(int i=1;i<=this->rows;i++){
+				for(int j=1;j<=b.cols;j++){
+					result.data[i][j]=0;
+					for(int k=1;k<=this->cols;k++){
+						result.data[i][j]=result.data[i][j]+ (this->	data[i][k]*b.data[k][j]);
+					}
+
+				}
+			}
+			return result;
+		}
+
 
 		
 	
 };
-
-
-
-vector< vector<double> > matrixMult(vector< vector<double> > &matrix1,vector< vector<double> > &matrix2,
- int rows1,int cols1,int rows2,int cols2){
-	vector< vector<double> > result;
-	createMatrix(result,rows1,cols2);
-	for(int i=1;i<=rows1;i++){
-
-		for(int j=1;j<=cols2;j++){
-			result[i][j]=0;
-			for(int k=1;k<=cols1;k++){
-				result[i][j]=result[i][j]+ (matrix1[i][k]*matrix2[k][j]);
-			}
-
-		}
-	}
-	return result;
-}
-
 
 int main(int argc, char const *argv[])
 {
@@ -82,6 +90,8 @@ int main(int argc, char const *argv[])
 	A.printMatrix();
 	Matrix B("2.txt");
 	B.printMatrix();
+	Matrix C=A.matrixMult(B);
+	C.printMatrix();
 
 	return 0;
 }
